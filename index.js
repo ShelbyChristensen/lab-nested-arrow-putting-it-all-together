0cf75d94614156f563e94f3524b2a32b84df8ed3
Shelby Christensen <shelbchrist@yahoo.com>
2025-05-12 13:41:46 -0500

    Implemented Login Feature to allow a user to attempt login a limited number of times before account locks

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
 
+    const loginAttempt = (passwordAttempt) => {
+        attemptCount++;
 
+        if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+            return "Login successful";
+        } else if (attemptCount <= 3) {
+            return `Attempt ${attemptCount}: Login failed`;
+        } else {
+            return "Account locked due to too many failed login attempts";
+        }
+    };
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+    return loginAttempt;
+}
+
+const tracker = createLoginTracker({ username: "Shelby_Christensen", password: "12345" });
+
+console.log(tracker("wrong"));
+console.log(tracker("nope"));
+console.log(tracker("12345"));
+console.log(tracker("12345"));
